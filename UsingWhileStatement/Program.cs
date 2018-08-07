304acfa54c31ea5710751912937e24047ad359ce
shubhamsoni6050 <40332828+shubhamsoni6050@users.noreply.github.com>
2018-08-07 18:19:24 +0530

    This is our second commit

---
+++ UsingWhileStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0, r;
+            Console.WriteLine("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                r = num % 10;
+                num = num / 10;
+                sum = sum + r;
+            }
+            Console.WriteLine("Sum of digits of the numbers : " + sum);
+           Console.ReadLine();
+
+        }
+    }
+}
+
